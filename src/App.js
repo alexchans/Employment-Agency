b29a54179324a856eb874ff9a027721b32708216
Alex Chen <alexchen@smu.edu>
2024-02-05 17:05:29 -0600

    Attempt for navigation

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './img/logo.png';
 import './App.css';
-import React from 'react';
 import Button from '@mui/material/Button';
+import ESignIn from './ESignIn';
+import PSignIn from './PSignIn';
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} alt="Logo" className="logo" />
-      <h1>Employment Agency</h1>
-      <h2>A recruiting hub that bring talent together!</h2>
-      <h3>Connect with job seekers and employees</h3>
-      <h1>Log In</h1>
-      <div className="input-container">
-        <label className="label" htmlFor="username">Username:</label>
-        <div>
-          <input type="text" id="username" name="username" />
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/EmployerSignIn" element={<ESignIn />} />
+          <Route path="/ProfessionalSignIn" element={<PSignIn />} />
+        </Routes>
+        <img src={logo} alt="Logo" className="logo" />
+        <h1>Employment Agency</h1>
+        <h2>A recruiting hub that brings talent together!</h2>
+        <h3>Connect with job seekers and employees</h3>
+        <h1>Log In</h1>
+        <div className="input-container">
+          <label className="label" htmlFor="username">Username:</label>
+          <div>
+            <input type="text" id="username" name="username" />
+          </div>
+          <label className="label" htmlFor="password">Password:</label>
+          <div>
+            <input type="text" id="password" name="password" />
+          </div>
         </div>
-        <label className="label" htmlFor="password">Password:</label>
-        <div>
-          <input type="text" id="password" name="password" />
+        <div className='button'>
+          <Button variant="contained">Log In</Button>
+        </div>
+        <hr style={{ width: '40%' }} />
+        <h4>Don't have an account yet?</h4>
+        <div className='sign-up'>
+          <Link to="/ProfessionalSignIn">Professional Sign Up</Link>
+          <span style={{ padding: 5 }}>|</span>
+          <Link to="/EmployerSignIn">Employer Sign Up</Link>
         </div>
       </div>
-      <div className='button'>
-        <Button variant="contained">Log In</Button>
-      </div>
-      <hr style={{ width: '40%' }} />
-      <h4>Don't have an account yet?</h4>
-      <div className='sign-up'>
-        <a href='www.google.com'>Professional Sign Up</a>
-        <span style={{ padding: 5 }}>|</span>
-        <a href='www.google.com'>Employer Sign Up</a>
-      </div>
-    </div >
+    </Router>
   );
 }
 
