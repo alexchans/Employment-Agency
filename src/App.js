6c351b29cbdc85ab4d7e6018da28f4dfbbdcc723
Alex Chen <alexchen@smu.edu>
2024-02-05 00:55:00 -0600

    Added logo, completed login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import logo from './img/logo.png';
 import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hello
-        </a>
-      </header>
-    </div>
+      <img src={logo} alt="Logo" className="logo" />
+      <h1>Employment Agency</h1>
+      <h2>A recruiting hub that bring talent together!</h2>
+      <h3>Connect with job seekers and employees</h3>
+      <h1>Log In</h1>
+      <div className="input-container">
+        <label className="label" htmlFor="username">Username:</label>
+        <div>
+          <input type="text" id="username" name="username" />
+        </div>
+        <label className="label" htmlFor="password">Password:</label>
+        <div>
+          <input type="text" id="password" name="password" />
+        </div>
+      </div>
+      <div className='button'>
+        <Button variant="contained">Log In</Button>
+      </div>
+      <hr style={{ width: '40%' }} />
+      <h4>Don't have an account yet?</h4>
+      <div className='sign-up'>
+        <a href='www.google.com'>Professional Sign Up</a>
+        <span style={{ padding: 5 }}>|</span>
+        <a href='www.google.com'>Employer Sign Up</a>
+      </div>
+    </div >
   );
 }
 
