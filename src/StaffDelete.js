f00a74a09bf425f5ddcbb3ed569aec1781ee06bd
Alex Chen <alexchen@smu.edu>
2024-04-23 18:20:45 -0500

    finished staffdelete

--- src/StaffDelete.js
+++ src/StaffDelete.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import StaffPageTemp from './components/StaffPageTemp';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
@@ -6,76 +6,83 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-class StaffDelete extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            openDialog: false,
-            selectedUser: null
+function StaffDelete() {
+    const [openDialog, setOpenDialog] = useState(false);
+    const [usernames, setUsernames] = useState([]);
+    const [balance, setbalance] = useState(null);
+
+    useEffect(() => {
+        const fetchUsernames = async () => {
+            const response = await axios.get(`http://172.24.240.1:8080/api/employer-deletion-requests/all`);
+            const employerUsernames = response.data.map(request => request.username);
+            const response2 = await axios.get(`http://172.24.240.1:8080/api/professionals/deletion-requests/all`);
+            const professionalUsernames = response2.data.map(request => request.username);
+            const usernames = [...employerUsernames, ...professionalUsernames];
+            setUsernames(usernames);
         };
-    }
 
-    handleOpenDialog = (user) => {
-        this.setState({
-            openDialog: true,
-            selectedUser: user
-        });
+        fetchUsernames();
+    }, []);
+
+    const handleApprove = async (username) => {
+        await axios.put(`http://172.24.240.1:8080/api/professionals/deletion/approve/${username}`);
+        await axios.put(`http://172.24.240.1:8080/api/employer-deletion-requests/approve/${username}`);
     };
 
-    handleCloseDialog = () => {
-        this.setState({
-            openDialog: false,
-            selectedUser: null
-        });
+    const handleOpenDialog = async (user) => {
+        setOpenDialog(true);
+        const response = await axios.get(`http://172.24.240.1:8080/api/payments/${user}/balance`);
+        setbalance(response.data);
     };
 
-    render() {
-        return (
-            <div>
-                <StaffPageTemp />
-                <div className='flex'>
-                    <ul>
-                        <li><Link to="/StaffUpdateInfo">Update Info</Link></li>
-                        <li><Link to="/StaffReviewERequests">Review E Requests</Link></li>
-                        <li><Link to="/StaffReviewPRequests">Review P Requests</Link></li>
-                        <li><Link to="/StaffDeleteAccounts">Delete Accounts</Link></li>
-                        <li><Link to="/StaffViewEAccounts">View E Accounts</Link></li>
-                        <li><Link to="/StaffViewPAccounts">View P Accounts</Link></li>
-                        <li><Link to="/StaffMatching">Initiate Matching</Link></li>
-                        <li><Link to="/StaffAddStaff">Add Staff</Link></li>
-                        <li><Link to="/StaffChangePassword">Change Password</Link></li>
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+        setbalance(null);
+    };
+
+    return (
+        <div>
+            <StaffPageTemp />
+            <div className='flex'>
+                <ul>
+                    <li><Link to="/StaffUpdateInfo">Update Info</Link></li>
+                    <li><Link to="/StaffReviewERequests">Review E Requests</Link></li>
+                    <li><Link to="/StaffReviewPRequests">Review P Requests</Link></li>
+                    <li><Link to="/StaffDeleteAccounts">Delete Accounts</Link></li>
+                    <li><Link to="/StaffViewEAccounts">View E Accounts</Link></li>
+                    <li><Link to="/StaffViewPAccounts">View P Accounts</Link></li>
+                    <li><Link to="/StaffMatching">Initiate Matching</Link></li>
+                    <li><Link to="/StaffAddStaff">Add Staff</Link></li>
+                    <li><Link to="/StaffChangePassword">Change Password</Link></li>
+                </ul>
+                <div style={{ paddingRight: '55vw' }}>
+                    <h2>Review Delete Account Requests</h2>
+                    <ul style={{ listStyleType: 'none', padding: '0' }}>
+                        {usernames.map(user => (
+                            <div key={user} className='flex' style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '10px', borderBottom: '1px solid #ccc', padding: '10px' }}>
+                                <li style={{ flex: 1, cursor: 'pointer', marginRight: '10px' }} onClick={() => handleOpenDialog(user)}>{user}</li>
+                                <div>
+                                    <Button onClick={() => handleApprove(user)} variant="contained" size='small'>Approve</Button>
+                                    <Button variant="contained" size='small' style={{ backgroundColor: 'red', marginLeft: '5px' }}>Deny</Button>
+                                </div>
+                            </div>
+                        ))}
                     </ul>
-                    <div style={{ paddingRight: '58vw' }}>
-                        <h2>Review Delete Account Requests</h2>
-                        <ul>
-                            <div className='flex' >
-                                <li onClick={() => this.handleOpenDialog('Alex Chen')}>Alex Chen</li>
-                                <Button variant="contained" size='small'>Approve</Button>
-                                <Button variant="contained" size='small' style={{ backgroundColor: 'red' }}>Deny</Button>
-                            </div>
-                            <hr />
-                            <div className='flex'>
-                                <li onClick={() => this.handleOpenDialog('Logan Lu')}>Logan Lu</li>
-                                <Button variant="contained" size='small'>Approve</Button>
-                                <Button variant="contained" size='small' style={{ backgroundColor: 'red' }}>Deny</Button>
-                            </div>
-                            <hr />
-                        </ul>
-                    </div>
                 </div>
-                <Dialog open={this.state.openDialog} onClose={this.handleCloseDialog}>
-                    <DialogTitle>User Details</DialogTitle>
-                    <DialogContent>
-                        <p>Account Balance: {this.state.selectedUser === 'Alex Chen' ? '0' : '-200'}</p>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={this.handleCloseDialog} color="primary">Close</Button>
-                    </DialogActions>
-                </Dialog>
             </div>
-        );
-    }
+            <Dialog open={openDialog} onClose={handleCloseDialog}>
+                <DialogTitle>User Details</DialogTitle>
+                <DialogContent>
+                    <p>Account Balance: {balance}</p>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDialog} color="primary">Close</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
 
 export default StaffDelete;
